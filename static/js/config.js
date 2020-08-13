afd6245d1255db3e4b0ec9b1240d5bf369ada541
wjkaliman <wjkaliman@gmail.com>
2020-08-13 13:25:00 -0700

    adding file and removing unessary files

--- static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key- const is a variable that never changes
-const API_KEY = "pk.eyJ1Ijoid2prYWxpbWFuIiwiYSI6ImNrZGo0NzEyeDAwMGkyeXBmdXkxeTBoYnIifQ.yFKGf0PMlSkh7KZUJ3xAvA";
--- templates/Static/js/logic.js
+++ templates/Static/js/logic.js
@@ -8,3 +8,31 @@ d3.json("http://localhost:5000/api/landing_data")
     console.log(error);
 });
 
+d3.json("http://localhost:5000/api/landing_data")
+    .then(function (response) {
+        console.log(response.data);
+        createMap(response.data.GeoLocation)
+     })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
+
+// Create the createMap function
+function createMap(GeoLocation) {
+    var satellitemap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/satellite-v9',
+        accessToken: API_KEY
+    });
+  
+
+    var myMap = L.map("map-id", {
+        center: [37.74, -25.68],
+        zoom: 2,
+        layers: [satellitemap]
+    });
+    lightMap.addTo(myMap)
+   };
