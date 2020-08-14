86d093d1b66ac5711157466b1efc5abbefdb5f92
wjkaliman <wjkaliman@gmail.com>
2020-08-14 14:28:58 -0700

    Added code to files

---
+++ templates/Static/js/plots.js
@@ -0,0 +1,86 @@
+console.log("hey you");
+
+d3.json("http://localhost:5000/api/landing_data")
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
+
+
+
+var trace1 = {
+    x: [0]["mass (g)"],
+    y: [0].recclass,
+    type: "bar"
+};
+
+var data = [trace1];
+
+var layout = {
+    title: "Test Chart",
+    xaxis: { title: "size" },
+    yaxis: { title: "Class" }
+};
+
+Plotly.newPlot("plot", data, layout);
+// //------------------------------------------------------------
+var trace2 = {
+  x: [[0].recclass],
+  y: [[0].year],
+  type: "line"
+};
+
+var data = [trace1];
+
+var layout = {
+  title: "Test Chart",
+  xaxis: { title: "size" },
+  yaxis: { title: "Year" }
+};
+
+Plotly.newPlot("scat", data, layout);
+// //---------------------------------------------------------------
+
+// // Scatter plot 1
+ var search1 = {
+    x: [0]["mass (g)"],
+    y: [0].recclass,
+    x: [],
+    y: [],
+    mode: 'markers',
+    type: 'scatter',
+    name: 'a',
+    text: ['', '', '', '', ''],
+    marker: { size: 12 }
+  };
+  
+  var search2 = {
+    x: [0].recclass,
+    y: [0].year,
+    mode: 'markers',
+    type: 'scatter',
+    name: 'B',
+    text: ['', '', '', '', ''],
+    marker: { size: 12 }
+  };
+  
+  var data = [ search1, search2 ];
+  
+  var layout = {
+    xaxis: {
+      range: [ 0.75, 2000 ] //use for grams?
+    },
+    yaxis: {
+      range: [0, 100] // use for ??
+    },
+    title:'Meteorite hits',
+    xaxis: { title: "size" },
+    yaxis: { title: "Class" }
+  };
+  
+  Plotly.newPlot('myDiv', data, layout);
+ // -----------------------------------------
